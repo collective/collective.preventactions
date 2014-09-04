a655ac8cfea660f03a89e7853ca06cb3e8cac26d
Benoît Suttor <benoit.suttor@imio.be>
2014-09-04 16:52:54 +0200

    check if marker interface is put on object when it deleted

--- src/collective/preventdelete/prevent.py
+++ src/collective/preventdelete/prevent.py
@@ -3,6 +3,9 @@ from OFS.interfaces import IItem
 from zope.container.interfaces import IObjectRemovedEvent
 from zope.component import adapter
 from plone.app.linkintegrity.interfaces import ILinkIntegrityInfo
+from collective.preventdelete.interfaces import IPreventDeleteActivated
+from collective.preventdelete import _
+from plone import api
 
 
 @adapter(IItem, IObjectRemovedEvent)
@@ -11,3 +14,11 @@ def deleteObject(obj, event):
     info = ILinkIntegrityInfo(request)
     if info.integrityCheckingEnabled():
         pass
+    if IPreventDeleteActivated.providedBy(obj):
+        msg = _(u"You can not delete this object")
+        api.portal.show_message(
+            message=msg,
+            request=request,
+            type="info"
+        )
+        request.response.redirect(obj.absolute_url())
